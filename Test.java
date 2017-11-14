24a692f9a29b80a76119949bd6e2466cc5f368d7
rajareddy0473 <rajareddy0473@gmail.com>
2017-11-14 09:21:47 +0530

    mofdi

--- Test.java
+++ Test.java
@@ -4,10 +4,14 @@ class Test
 	public static void m1(){
 System.out.println("m1 method");
 	}
+public static void m2(){
+System.out.println("m2 method");
+	}
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
 		m1();
+m2();
 	}
 }
